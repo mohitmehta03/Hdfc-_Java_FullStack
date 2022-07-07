69c0d6120f893da79a399d615c4f3eca80a8506b
Mohit Mehta <m.mehta03@outlook.com>
2022-07-07 13:43:19 +0530

    version 1.0

---
+++ gitrepo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class gitrepo
+{
+  public static void main(String[] args)
+  {
+    Scanner s = new Scanner(System.in);
+    System.out.print("input your name : ");
+    
+  }
+}
